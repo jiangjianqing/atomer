c979b7ff6d607ff53d2fbd7eaff7c2835bbf9fe8
jjq <cz_jjq@163.com>
2017-04-24 15:04:47 +0800

    upgrade

--- test/testcase/test.js
+++ test/testcase/test.js
@@ -1,8 +1,14 @@
 import assert from 'assert';
 require('chai').should(); //注意：使用should()后会污染js中对象的原型对象
 var expect=require('chai').expect;
+var fs = require('fs');
+console.log(process.cwd());
+
+//var atomerConfig = JSON.parse(fs.readFileSync("atomer-example.json"));
 
 //注意：模块最终发布的是编译后的程序，为了避免因babel的Bug而导致编译后的程序与源程序功能有差异，单元测试需要改用编译后的代码。
+
+//console.log(atomerConfig);
 
 describe('es2015_demo', () => {
 
