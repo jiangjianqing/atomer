74e374a9e22e7ef15469311bcdeebfdb40cf749e
jjq <cz_jjq@163.com>
2017-04-24 14:31:38 +0800

    upgrade

--- test/test.js
+++ test/test.js
@@ -1,15 +1,20 @@
 import assert from 'assert';
-import path from 'path';
-import fs from 'fs';
+require('chai').should(); //注意：使用should()后会污染js中对象的原型对象
+var expect=require('chai').expect;
 
-//注意：模块最终发布的却是编译后的程序，为了避免因babel的Bug而导致编译后的程序与源程序功能有差异，我们的单元测试需要改用编译后的代码。
+//注意：模块最终发布的是编译后的程序，为了避免因babel的Bug而导致编译后的程序与源程序功能有差异，单元测试需要改用编译后的代码。
 
 describe('es2015_demo', () => {
 
     it('复制本地文件成功', done => {
 
         assert.equal(1, 1);
-
+        "test".should.have.length(4); //should已经属于原型方法
+        var foo ="bar";
+        expect(foo).to.be.a('string');
+        expect(foo).to.equal('bar');
+        expect(foo).to.have.length(3);
+        //expect(beverages).to.have.property('tea').with.length(3);
         done();
     });
 
