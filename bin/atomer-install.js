cc9307ec515a44ae0e637ce9fd7d071d143b2a52
cz_jjq <cz_jjq@163.com>
2017-05-06 21:20:58 +0800

    upgrade

--- bin/atomer-install.js
+++ bin/atomer-install.js
@@ -4,6 +4,7 @@
 
 var program = require("commander");
 var atomPackage= require("../lib/atom-package");
+var util = require("util");
 
 var downloadPackage = require("./features/download-package");
 var unpackToNodeModules = require("./features/unpack-to-node-modules");
@@ -19,44 +20,48 @@ program
 
 program.parse(process.argv);
 
-var config = atomPackage.load();
-if(packageName){
-
-    config.dependencies[packageName] = "latest";
-    atomPackage.save(config);
-
+var installPackage = function(pkgName,successCallback){
     var s = new Series();
     s.init([
-        function (callback, pkgName) {
+        function(callback, pkgName){
             downloadPackage(pkgName,callback);
         },
         function(callback, tarFileName, repoName){
-            unpackToNodeModules(tarFileName, repoName, function(){
-                console.log("unpack package [ %s  ] ok!",tarFileName);
-            });
+            unpackToNodeModules(tarFileName, repoName, callback);
+        },
+        function(callback, targetDir, repoName){
+            console.log("unpack package [ %s  ] ok!",repoName);
+            execNpmInstall(targetDir);
+            if(successCallback){
+                successCallback();
+            }
         }
     ]);
 
-    s.run(packageName);
-    //todo:下载包并安装到node_modules
+    s.fail(function(error){
+        console.log("download package [%s] failed!", pkgName);
+        if(successCallback){
+            successCallback(error);
+        }
+    });
+
+    s.run(pkgName);
+};
+
+var config = atomPackage.load();
+if(packageName){
+
+    if (!util.isObject(config["dependencies"])){
+        config["dependencies"] = {};
+    }
+
+    config["dependencies"][packageName] = "latest";
+    atomPackage.save(config);
+
+    installPackage(packageName);
 }else{
     for(var pkgName in config["dependencies"]) {
-        var s = new Series();
-        s.init([
-            function(callback, pkgName){
-                downloadPackage(pkgName,callback);
-            },
-            function(callback, tarFileName, repoName){
-                unpackToNodeModules(tarFileName, repoName, callback);
-            },
-            function(callback, targetDir, repoName){
-                console.log("unpack package [ %s  ] ok!",repoName);
-                execNpmInstall(targetDir);
-            }
-        ]);
-
-        s.run(pkgName);
-
+        installPackage(pkgName);
     }
 }
 
